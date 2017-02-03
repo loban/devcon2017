34d9c60c45d763cc7382ed30d1c59f67c37854b1
Loban Rahman <loban.rahman@gmail.com>
2017-02-03 08:48:18 +0600

    Made the code simpler by removing unnecessary data in the loaded file.

--- src/devcon2017-1.sync.js
+++ src/devcon2017-1.sync.js
@@ -54,9 +54,9 @@ function syncTask() {
     debug('jsonContent', jsonContent);
 
     let httpContent = jsonContent.map(function (item) {
-      debug('loading item', item.url);
-      let httpResponse = request('GET', item.url).getBody('utf-8');
-      return httpResponse.substr(item.position, item.length);
+      debug('loading item', item);
+      let httpResponse = request('GET', item).getBody('utf-8');
+      return httpResponse.substr(0, 10);
     });
 
     debug('httpContent', httpContent);
